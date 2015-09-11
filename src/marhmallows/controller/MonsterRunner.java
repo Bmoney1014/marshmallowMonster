83af5b0874c8b87927cdacbaec103928a1f7bf6f
Brock Hostetter <bstetterslc@gmail.com>
2015-09-11 08:12:55 -0600

    My Marshmallow Monster

---
+++ src/marhmallows/controller/MonsterRunner.java
@@ -0,0 +1,11 @@
+package marhmallows.controller;
+
+public class MonsterRunner
+{
+	public static void main (String [] args)
+	{
+		MonstereController myController = new MonstereController();
+		myController.start();
+	}
+
+}
